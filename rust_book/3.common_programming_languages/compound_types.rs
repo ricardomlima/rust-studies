5b3b14f9b2b1cf9887c6752e834cd61ecc83d7d1
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-01 14:26:56 -0300

    compound types

---
+++ rust_book/3.common_programming_languages/compound_types.rs
@@ -0,0 +1,15 @@
+fn main() {
+    
+    /* TUPLE ARRAY 
+    
+    can store different types
+    */
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // retrieving elements from a tuple
+    let (x, y, z) = tup;
+
+    println!("The value of y is {y}");
+
+
+}
