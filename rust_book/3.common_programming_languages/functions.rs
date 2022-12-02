30e8bb739ca397482d62078c9a6b2767afec7d98
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-01 22:40:09 -0300

    basic statements and expressions

--- rust_book/3.common_programming_languages/functions.rs
+++ rust_book/3.common_programming_languages/functions.rs
@@ -1,12 +1,36 @@
 /* FUNCTIONS
 
 Names are snake case!
+
 */
 
 fn main() {
     println!("main function");
     another_function();
     another_function_parameters(15, 'm');
+
+    /*
+     Statements do not return value.
+     E.g:
+    */
+    let me = "test"; // this does not return a value. Not even the assignment value.
+
+    /*
+     because statemements don't return values we can't do this like in other languages:
+     let x = y = 6;
+    */
+
+    /*
+     Expressions return values! Expressions can be part of statements.
+     Expressions do not include ending semicolons. If you add an ending
+     semicolon you turn the expression into a statement and then it stops
+     returning
+    */
+    let y_block = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y_block is: {y_block}");
 }
 
 fn another_function() {
