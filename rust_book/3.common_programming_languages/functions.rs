a13a0a8fb17d4e55353a437d766f786b2be956fa
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-01 22:20:02 -0300

    multiple parameters

--- rust_book/3.common_programming_languages/functions.rs
+++ rust_book/3.common_programming_languages/functions.rs
@@ -6,13 +6,14 @@ Names are snake case!
 fn main() {
     println!("main function");
     another_function();
-    another_function_parameters(15);
+    another_function_parameters(15, 'm');
 }
 
 fn another_function() {
     println!("Another function");
 }
 
-fn another_function_parameters(x: i32) {
-    println!("The value of x is {x}")
+// in functions parameters must be declared with types!
+fn another_function_parameters(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}")
 }
