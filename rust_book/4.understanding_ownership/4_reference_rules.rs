4de191197485dc6fdeaaeb92f3bceadf25e630c1
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-22 16:04:55 -0300

    dangling references

--- rust_book/4.understanding_ownership/4_reference_rules.rs
+++ rust_book/4.understanding_ownership/4_reference_rules.rs
@@ -10,4 +10,19 @@ fn main() {
     // have already been used. No risk of changing.
     // Users of an immutable reference don’t expect the value to suddenly change out from under them!
     println!("{}", r3);
+
+    let reference_to_nothing = dangle();
+
+    let reference_to_something = no_dangle();
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+    &s // returns reference to a string that is dropped! Because this function owns it.
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s // returns the string and moves the ownership to the caller.
+    //No risk of it being dropped before being used outside!
 }
