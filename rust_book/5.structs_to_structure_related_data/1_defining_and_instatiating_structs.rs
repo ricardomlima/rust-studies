d129418b359181f17883d7c82b054919b5842669
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-26 16:59:17 -0300

    using struct and struct update syntax

---
+++ rust_book/5.structs_to_structure_related_data/1_defining_and_instatiating_structs.rs
@@ -0,0 +1,24 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+fn main() {
+    let user1 = User {
+        email: String::from("email@gmail.com"),
+        username: String::from("username"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("The username is {}", user1.username);
+
+    // using the struct update syntax
+    let user2 = User {
+        email: String::from("anotheremail@gmail.com"),
+        ..user1
+    };
+
+    println!("The other username is {}", user2.username);
+}
