a78295f2ffdb5da6971db522eb2201a1136803b3
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-26 17:59:12 -0300

    associated function again

--- rust_book/5.structs_to_structure_related_data/4_method_syntax.rs
+++ rust_book/5.structs_to_structure_related_data/4_method_syntax.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other_rectangle: &Rectangle) -> bool {
+        self.width > other_rectangle.width && self.height > other_rectangle.height
+    }
 }
 
 fn main() {
@@ -15,5 +19,19 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    let rect2 = Rectangle {
+        width: 15,
+        height: 25,
+    };
+
+    let rect3 = Rectangle {
+        width: 90,
+        height: 100,
+    };
     println!("The area of the rectangle is {}", rect1.area());
+
+    println!("it is {} that rectangle 1 can hold rectangle 2", rect1.can_hold(&rect2));
+
+    println!("it is {} that rectangle 1 can hold rectangle 3", rect1.can_hold(&rect3));
 }
