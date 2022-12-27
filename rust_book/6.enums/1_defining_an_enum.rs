9562174883af1e668ac840ae8366c4eece17a221
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-27 15:32:59 -0300

    using enum with structs

--- rust_book/6.enums/1_defining_an_enum.rs
+++ rust_book/6.enums/1_defining_an_enum.rs
@@ -3,7 +3,22 @@ enum IpAddrKind {
     v6,
 }
 
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
     let four = IpAddrKind::v4;
     let six = IpAddrKind::v6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
