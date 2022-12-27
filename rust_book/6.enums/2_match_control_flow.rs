03b5438d448dff9b661209d798fa448a91cff563
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-27 16:52:31 -0300

    a bit more about pattern matching

--- rust_book/6.enums/2_match_control_flow.rs
+++ rust_book/6.enums/2_match_control_flow.rs
@@ -9,4 +9,19 @@ fn main() {
     let five = Some(5);
     let _six = plus_one(five);
     let _none = plus_one(None);
+
+    let dice_roll = 9;
+
+    match dice_roll {
+        3 => {
+            println!("You won a hat!");
+        }
+        7 => {
+            println!("You won some cash");
+        }
+        other => {
+            // you can use _ instead of other in case you won't use the variable!
+            println!("you move {}", other);
+        }
+    }
 }
